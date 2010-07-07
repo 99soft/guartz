72009db4821cd628b72819496b687bd3f82234f3
simone.tripodi <simone.tripodi@a9d04f66-3055-11df-a735-6ff41f51f3aa>
2010-07-07 09:33:32 +0000

    first checkin of InjectorJobFactory class it allows guice creating Job classes, so Jobs support @Inject annotations
    git-svn-id: http://slurry.googlecode.com/svn/quartz4guice/trunk@105 a9d04f66-3055-11df-a735-6ff41f51f3aa

---
+++ src/main/java/org/slurry/quartz4guice/scheduling/InjectorJobFactory.java
@@ -0,0 +1,47 @@
+/*
+ *    Copyright 2009-2010 The slurry Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.slurry.quartz4guice.scheduling;
+
+import org.quartz.Job;
+import org.quartz.SchedulerException;
+import org.quartz.spi.JobFactory;
+import org.quartz.spi.TriggerFiredBundle;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+
+/**
+ * 
+ * @version $Id$
+ */
+@Singleton
+public final class InjectorJobFactory implements JobFactory {
+
+    private final Injector injector;
+
+    @Inject
+    public InjectorJobFactory(Injector injector) {
+        this.injector = injector;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Job newJob(TriggerFiredBundle bundle) throws SchedulerException {
+        Class<? extends Job> jobClass = bundle.getJobDetail().getJobClass();
+        return this.injector.getInstance(jobClass);
+    }
+
+}
