a3ec8be45f6b8c7b862811ff367a99566828efeb
simone.tripodi <simone.tripodi@a9d04f66-3055-11df-a735-6ff41f51f3aa>
2010-07-07 14:48:06 +0000

    added support for listeners
    git-svn-id: http://slurry.googlecode.com/svn/quartz4guice/trunk@125 a9d04f66-3055-11df-a735-6ff41f51f3aa

--- src/main/java/org/slurry/quartz4guice/module/ScheduleModule.java
+++ src/main/java/org/slurry/quartz4guice/module/ScheduleModule.java
@@ -15,22 +15,41 @@
  */
 package org.slurry.quartz4guice.module;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.quartz.JobListener;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerFactory;
+import org.quartz.SchedulerListener;
+import org.quartz.TriggerListener;
 import org.quartz.spi.JobFactory;
 import org.slurry.quartz4guice.aop.ScheduledTypeListener;
-import org.slurry.quartz4guice.scheduling.InjectorJobFactory;
 import org.slurry.quartz4guice.scheduling.DefaultSchedulerFactoryProvider;
+import org.slurry.quartz4guice.scheduling.InjectorJobFactory;
 import org.slurry.quartz4guice.scheduling.SchedulerProvider;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Binder;
 import com.google.inject.Provider;
+import com.google.inject.binder.ScopedBindingBuilder;
+import com.google.inject.multibindings.Multibinder;
 
 /**
  * 
  * @version $Id$
  */
 public final class ScheduleModule extends AbstractModule {
+
+    private final Set<Class<? extends JobListener>> globalJobListeners = new HashSet<Class<? extends JobListener>>();
+
+    private final Set<Class<? extends JobListener>> jobListeners = new HashSet<Class<? extends JobListener>>();
+
+    private final Set<Class<? extends TriggerListener>> globalTriggerListeners = new HashSet<Class<? extends TriggerListener>>();
+
+    private final Set<Class<? extends TriggerListener>> triggerListeners = new HashSet<Class<? extends TriggerListener>>();
+
+    private final Set<Class<? extends SchedulerListener>> schedulerListeners = new HashSet<Class<? extends SchedulerListener>>();
 
     private final Class<? extends Provider<SchedulerFactory>> schedulerFactoryProviderClass;
 
@@ -42,6 +61,31 @@ public final class ScheduleModule extends AbstractModule {
         this.schedulerFactoryProviderClass = schedulerFactoryProviderClass;
     }
 
+    public ScheduleModule addGlobalJobListener(Class<? extends JobListener> jobListenerType) {
+        return this.addElement(this.globalJobListeners, jobListenerType);
+    }
+
+    public ScheduleModule addJobListener(Class<? extends JobListener> jobListenerType) {
+        return this.addElement(this.jobListeners, jobListenerType);
+    }
+
+    public ScheduleModule addGlobalTriggerListener(Class<? extends TriggerListener> triggerListenerType) {
+        return this.addElement(this.globalTriggerListeners, triggerListenerType);
+    }
+
+    public ScheduleModule addTriggerListener(Class<? extends TriggerListener> triggerListenerType) {
+        return this.addElement(this.triggerListeners, triggerListenerType);
+    }
+
+    public ScheduleModule addSchedulerListener(Class<? extends SchedulerListener> schedulerListenerType) {
+        return this.addElement(this.schedulerListeners, schedulerListenerType);
+    }
+
+    private <I> ScheduleModule addElement(Set<Class<? extends I>> set, Class<? extends I> type) {
+        set.add(type);
+        return this;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -50,7 +94,25 @@ public final class ScheduleModule extends AbstractModule {
         bind(JobFactory.class).to(InjectorJobFactory.class);
         bind(Scheduler.class).toProvider(SchedulerProvider.class);
 
+        bind(this.binder(), this.globalJobListeners, JobListener.class, true);
+        bind(this.binder(), this.jobListeners, JobListener.class, false);
+        bind(this.binder(), this.schedulerListeners, SchedulerListener.class, false);
+        bind(this.binder(), this.globalTriggerListeners, TriggerListener.class, true);
+        bind(this.binder(), this.triggerListeners, TriggerListener.class, false);
+
         bindListener(new JobMatcher(), new ScheduledTypeListener());
+    }
+
+    private static <I> void bind(Binder binder, Set<Class<? extends I>> set, Class<I> type, boolean global) {
+        if (!set.isEmpty()) {
+            Multibinder<I> multibinder = Multibinder.newSetBinder(binder, type);
+            for (Class<? extends I> currentType : set) {
+                ScopedBindingBuilder bindingBuilder = multibinder.addBinding().to(currentType);
+                if (global) {
+                    bindingBuilder.in(Global.class);
+                }
+            }
+        }
     }
 
 }
