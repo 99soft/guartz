e05df2128463863d1c4e81412fbc8c9144547acb
simone.tripodi <simone.tripodi@a9d04f66-3055-11df-a735-6ff41f51f3aa>
2010-07-15 21:32:26 +0000

    first checkin of InstanceIdGenerator providers
    git-svn-id: http://slurry.googlecode.com/svn/quartz4guice/trunk@133 a9d04f66-3055-11df-a735-6ff41f51f3aa

---
+++ src/main/java/org/slurry/quartz4guice/spi/HostnameInstanceIdGeneratorProvider.java
@@ -0,0 +1,36 @@
+/*
+ *    Copyright 2009-2010 The slurry Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.slurry.quartz4guice.spi;
+
+import org.quartz.simpl.HostnameInstanceIdGenerator;
+import org.quartz.spi.InstanceIdGenerator;
+
+import com.google.inject.Provider;
+
+/**
+ * 
+ * @version $Id$
+ */
+public final class HostnameInstanceIdGeneratorProvider
+        implements Provider<InstanceIdGenerator> {
+
+    private final InstanceIdGenerator generator = new HostnameInstanceIdGenerator();
+
+    public InstanceIdGenerator get() {
+        return this.generator;
+    }
+
+}
