39303531977ea351bf3d83d77d5f945a024e9fda
simone.tripodi <simone.tripodi@a9d04f66-3055-11df-a735-6ff41f51f3aa>
2010-07-15 20:43:25 +0000

    first checkin of InitThreadContextClassLoadHelperProvider class
    git-svn-id: http://slurry.googlecode.com/svn/quartz4guice/trunk@131 a9d04f66-3055-11df-a735-6ff41f51f3aa

---
+++ src/main/java/org/slurry/quartz4guice/spi/InitThreadContextClassLoadHelperProvider.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2009-2010 The slurry Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.slurry.quartz4guice.spi;
+
+import org.quartz.simpl.InitThreadContextClassLoadHelper;
+import org.quartz.spi.ClassLoadHelper;
+
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+/**
+ * 
+ * @version $Id$
+ */
+@Singleton
+public final class InitThreadContextClassLoadHelperProvider
+        implements Provider<ClassLoadHelper> {
+
+    private final ClassLoadHelper classLoadHelper = new InitThreadContextClassLoadHelper();
+
+    public ClassLoadHelper get() {
+        return this.classLoadHelper;
+    }
+
+}
