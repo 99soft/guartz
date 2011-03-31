1614263263120b0b1b70cc164e3b0bd452dbab6d
simonetripodi <simone.tripodi@gmail.com>
2011-04-01 00:35:03 +0200

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.guice.guartz", "org/nnsoft/guice/guartz/package-summary.html", [ [ "Description", "org/nnsoft/guice/guartz/package-descr.html", null, "" ], [ "Classes", null, [ [ "JobSchedulerBuilder", "org/nnsoft/guice/guartz/JobSchedulerBuilder.html", null, "" ], [ "QuartzModule", "org/nnsoft/guice/guartz/QuartzModule.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "Scheduled", "org/nnsoft/guice/guartz/Scheduled.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
