79db58e32127fec01d1ff0910cdfb187bfb7f455
simonetripodi <simone.tripodi@gmail.com>
2012-01-27 12:11:57 +0100

    Wagon: Deploying site to repository

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,5 +1,6 @@
 var NAVTREE_DATA =
-[ [ "org.nnsoft.guice.guartz", "org/nnsoft/guice/guartz/package-summary.html", [ [ "Description", "org/nnsoft/guice/guartz/package-descr.html", null, "" ], [ "Classes", null, [ [ "JobSchedulerBuilder", "org/nnsoft/guice/guartz/JobSchedulerBuilder.html", null, "" ], [ "QuartzModule", "org/nnsoft/guice/guartz/QuartzModule.html", null, "" ] ]
+[ [ "org.nnsoft.guice.guartz", "org/nnsoft/guice/guartz/package-summary.html", [ [ "Description", "org/nnsoft/guice/guartz/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "SchedulerConfigurationBuilder", "org/nnsoft/guice/guartz/SchedulerConfigurationBuilder.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "JobSchedulerBuilder", "org/nnsoft/guice/guartz/JobSchedulerBuilder.html", null, "" ], [ "QuartzModule", "org/nnsoft/guice/guartz/QuartzModule.html", null, "" ] ]
 , "" ], [ "Annotations", null, [ [ "Scheduled", "org/nnsoft/guice/guartz/Scheduled.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -2,6 +2,7 @@ var DATA = [
       { id:0, label:"org.nnsoft.guice.guartz", link:"org/nnsoft/guice/guartz/package-summary.html", type:"package" },
       { id:1, label:"org.nnsoft.guice.guartz.JobSchedulerBuilder", link:"org/nnsoft/guice/guartz/JobSchedulerBuilder.html", type:"class" },
       { id:2, label:"org.nnsoft.guice.guartz.QuartzModule", link:"org/nnsoft/guice/guartz/QuartzModule.html", type:"class" },
-      { id:3, label:"org.nnsoft.guice.guartz.Scheduled", link:"org/nnsoft/guice/guartz/Scheduled.html", type:"class" }
+      { id:3, label:"org.nnsoft.guice.guartz.Scheduled", link:"org/nnsoft/guice/guartz/Scheduled.html", type:"class" },
+      { id:4, label:"org.nnsoft.guice.guartz.SchedulerConfigurationBuilder", link:"org/nnsoft/guice/guartz/SchedulerConfigurationBuilder.html", type:"class" }
 
     ];
